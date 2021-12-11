20a63b8a83607f6d047e2d0bc6b1543560b11dde
anonymous164 <j@jasonbreedlove.com>
2021-12-11 00:02:08 -0500

    added banner retrieval for each port that is scanned. added comments. stripped and cleaned banner results

--- portscanner.py
+++ portscanner.py
@@ -2,22 +2,59 @@ import socket
 from IPy import IP
 
 
+# scan the target and define the port range
+def scan(target):
+    # call the function that changes domain names into ip addresses
+    converted_ip = check_ip(target)
+    print('\n' + '[Scanning Target] ' + str(target))
+    # scan port 1-500
+    for port in range(1, 443):
+        scan_port(converted_ip, port)
+
+
+# use IPy to convert hostname to ip
+def check_ip(ip):
+    try:
+        IP(ip)
+        return ip
+    except ValueError:
+        # get the ip from the hostname
+        return socket.gethostbyname(ip)
+
+
+def get_banner(s):
+    return s.recv(1024)
+
+
+# Create socket and connect to ip and port before scanning ports
 def scan_port(ipaddress, port):
     # condition to test for open ports
     try:
         # create socket
         sock = socket.socket()
-        # set a timeout on the socket so it doesn't stall while running
+        # set a timeout on the socket, so it doesn't stall while running
         sock.settimeout(0.5)
+        # connect to port
         sock.connect((ipaddress, port))
-        print(f'[+] Port ' + str(port) + ' is Open')
-    # catches all closed ports
+        try:
+            banner = get_banner(sock)
+            print('[+] Open Port ' + str(port) + ':' + str(banner.decode().strip('\n')))
+        except:
+            print('[+] Open Port ' + str(port))
+    # catches all closed ports and passes so that only open ports are shown
     except:
-        print(f'[-] Port ' + str(port) + ' is Closed')
+        pass
 
-# enter the ip address to scan
-ipaddress = input('[+] Enter Target to Scan: ')
 
-# iterate range of ports to scan
-for port in range(75, 85):
-    scan_port(ipaddress, port)
+if __name__ == "__main__":
+    # prompt user for targets
+    targets = input('[+] Enter Target/s To Scan(split multiple targets with ,): ')
+    # if there is a comma after target then split the ip and strip any excess spaces
+    if ',' in targets:
+
+        # iterate to scan each ip address that is added
+        for ip_add in targets.split(','):
+            scan(ip_add.strip(' '))
+    # commence with scanning the listed targets
+    else:
+        scan(targets)
